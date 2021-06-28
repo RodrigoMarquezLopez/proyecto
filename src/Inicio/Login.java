99d9b7d0e08d47655947577fc1f7d9c3a3acea40
AlfredDev <56953358+AlfredDev@users.noreply.github.com>
2021-06-28 12:23:27 -0500

    Add files via upload

--- src/Inicio/Login.java
+++ src/Inicio/Login.java
@@ -16,16 +16,12 @@ import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Insets;
 import java.awt.Shape;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.geom.RoundRectangle2D;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 import javax.swing.SwingConstants;
 
@@ -33,12 +29,14 @@ import javax.swing.SwingConstants;
  *
  * @author Jose Alfredo Garcia Cortes
  */
-public class Login extends JFrame{
+public class Login extends JFrame {
+
     protected CuadroContr passwordField;
     protected JButton join;
     protected CuadroTex txuser;
     protected JButton close;
-    
+    protected JButton Nuevo;
+
     public Login() {
         this.setSize(820, 550);
         setUndecorated(true);
@@ -62,31 +60,31 @@ public class Login extends JFrame{
         main.setBackground(new Color(15, 157, 167));
         JLabel titulo = title("EL PALACIO DE LA");
         main.add(titulo);
-        s.putConstraint(SpringLayout.WEST, titulo,12, SpringLayout.WEST, main);
-        s.putConstraint(SpringLayout.NORTH,titulo, 22,SpringLayout.NORTH, main);
+        s.putConstraint(SpringLayout.WEST, titulo, 12, SpringLayout.WEST, main);
+        s.putConstraint(SpringLayout.NORTH, titulo, 22, SpringLayout.NORTH, main);
         JLabel titulo2 = title("MEMORIA");
         main.add(titulo2);
-        s.putConstraint(SpringLayout.NORTH,titulo2,40,SpringLayout.NORTH, titulo);
-        s.putConstraint(SpringLayout.WEST,titulo2,98,SpringLayout.WEST, main);
+        s.putConstraint(SpringLayout.NORTH, titulo2, 40, SpringLayout.NORTH, titulo);
+        s.putConstraint(SpringLayout.WEST, titulo2, 98, SpringLayout.WEST, main);
         ImageIcon imageIcon = new ImageIcon("src/imagenes/fondo.png");
         Image image = imageIcon.getImage();
         Image newimg = image.getScaledInstance(350, 480, Image.SCALE_SMOOTH);
         imageIcon = new ImageIcon(newimg);
         JLabel img = new JLabel(imageIcon);
         main.add(img);
-        s.putConstraint(SpringLayout.WEST, img,35,SpringLayout.WEST,main);
-        s.putConstraint(SpringLayout.NORTH, img,20,SpringLayout.NORTH,titulo2);
+        s.putConstraint(SpringLayout.WEST, img, 35, SpringLayout.WEST, main);
+        s.putConstraint(SpringLayout.NORTH, img, 20, SpringLayout.NORTH, titulo2);
         return main;
     }
 
-    public JLabel title(String leyenda){
-     JLabel titulo = new JLabel(leyenda);
+    public JLabel title(String leyenda) {
+        JLabel titulo = new JLabel(leyenda);
         titulo.setHorizontalAlignment(SwingConstants.CENTER);
         titulo.setFont(new Font("Arial Black", Font.BOLD, 35));
         titulo.setForeground(Color.WHITE);
         return titulo;
     }
-    
+
     public JPanel Log() {
         JPanel main = new JPanel(new BorderLayout());
         main.setBackground(new Color(33, 45, 62, 255));
@@ -138,25 +136,28 @@ public class Login extends JFrame{
         r.gridwidth = GridBagConstraints.REMAINDER;
         r.ipady = 5;
         passwordField = new CuadroContr(15);
-         formulario.add(passwordField, r);
-        r.insets = new Insets(60, 20, 50, 0);
+        formulario.add(passwordField, r);
+        r.insets = new Insets(15, 55, 15, 0);
         join = new JButton(new ImageIcon("src/imagenes/button_ingresar.png"));
         join.setBackground(new Color(33, 45, 62, 255));
         join.setBorder(null);
-        join.setActionCommand("ingresar");
+       // join.setActionCommand("ingresar");
         //join.addActionListener(this);
         r.fill = GridBagConstraints.HORIZONTAL;
-        r.gridwidth = GridBagConstraints.CENTER;
+        r.gridwidth = GridBagConstraints.REMAINDER;
         r.anchor = GridBagConstraints.EAST;
         formulario.add(join, r);
 
+        Nuevo = new JButton(new ImageIcon("src/imagenes/nuevo.png"));
+        Nuevo.setBorder(null);
+        Nuevo.setBackground(new Color(33, 45, 62, 255));
+        formulario.add(Nuevo,r);
         main.add(formulario, BorderLayout.CENTER);
         JPanel col = new JPanel();
         col.setBackground(new Color(0, 0, 0, 0));
         main.add(col, BorderLayout.SOUTH);
         return main;
     }
-
 
     public JLabel jLabel(String leyenda) {
         JLabel label = new JLabel(leyenda);
@@ -171,11 +172,15 @@ public class Login extends JFrame{
         l.ConectaControlador(c);
     }
 
-     public void ConectaControlador(ControladorLogin c){
-         join.setActionCommand("ingresar");
-         join.addActionListener(c);
-         close.setActionCommand("cerrar");
-         close.addActionListener(c);
+    public void ConectaControlador(ControladorLogin c) {
+        join.setActionCommand("ingresar");
+        join.addActionListener(c);
+        
+        Nuevo.setActionCommand("nuevo");
+        Nuevo.addActionListener(c);
+        
+        close.setActionCommand("cerrar");
+        close.addActionListener(c);
 
     }
 
